71facbcc391d93296c98584af53ece3d8fe93bab
fongelias <fongelias@gmail.com>
2016-03-31 17:56:26 -0400

    Finished side-navigation

--- js/main.js
+++ js/main.js
@@ -4,14 +4,21 @@ $(document).ready(function(){
 	/* ----------------Side Nav----------------------*/
 	var hamburger = $('#nav-burger');
 	var body = $("body");
+	var sideNavButton = $(".side-nav-button")
 
-	//Hamburger to expanded nav
+	//Click Hamburger to open Side nav
 	hamburger.click(function() {
 		hamburger.toggleClass('active');
 		body.toggleClass('active-side-nav');
 		//return false;
 	});
 
+	//Click Hamburger to open Side nav
+	sideNavButton.click(function() {
+		hamburger.toggleClass('active');
+		body.toggleClass('active-side-nav');
+		//return false;
+	});
 	/* ########################Reused Event Listeners below################################# */
 	/* ----------------Window width detection-----------*/
 	//Window width detection
